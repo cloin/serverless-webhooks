93d54a692a6832c1d69bb2b2035ed7db310ad659
Colin McNaughton <cloin@users.noreply.github.com>
2023-07-06 12:07:53 -0400

    validation

--- sqs.py
+++ sqs.py
@@ -4,9 +4,11 @@ import boto3
 import logging
 
 # These are set in environment variables
-# In lamda, set these in the Configuration tab
+# In lambda, set these in the Configuration tab
 QUEUE_URL = os.getenv('QUEUE_URL')
 REGION_NAME = os.getenv('REGION_NAME')
+VALIDATION_KEY = os.getenv('VALIDATION_KEY')
+VALIDATION_VALUE = os.getenv('VALIDATION_VALUE')
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -14,8 +16,15 @@ logger.setLevel(logging.INFO)
 sqs = boto3.client('sqs', region_name=REGION_NAME)
 
 def lambda_handler(event, context):
-    body = json.loads(event.get('body', '{}'))
     headers = event.get('headers', {})
+    
+    if VALIDATION_KEY not in headers or headers[VALIDATION_KEY] != VALIDATION_VALUE:
+        return {
+            'statusCode': 403,
+            'body': f"Validation failed. Invalid validation key or value provided in headers"
+        }
+
+    body = json.loads(event.get('body', '{}'))
 
     message = {
         'body': body,
